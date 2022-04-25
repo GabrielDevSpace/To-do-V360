fdf2b84a4983cfb60627ee8112cb8e6b578cfccf
GabrielPires <gabrielpirestj@hotmail.com>
2022-04-25 11:20:40 -0300

    Correções Navbar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -14,24 +14,27 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
                 <a class="navbar-brand" href="/">To Do List</a>
               </li>
-              <li class="nav-item pr-2">
+              <li class="nav-item pr-2 pb-1">
                 <a href="{{url('/')}}" class="btn btn-primary" title="Adicionar Novo Item">
-                    <span><i class="fa fa-home" aria-hidden="true"></i></span>
+                    <span><i class="fa fa-home" aria-hidden="true"></i> Pagina Inicial</span>
                 </a>
               </li>
-              <li class="nav-item dropdown pr-2">
+              <li class="nav-item pr-2 pb-1">
                 <a href="{{url('calendario')}}" class="btn btn-primary" title="Adicionar Novo To Do Lists">
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                    <i class="fa fa-calendar" aria-hidden="true"></i> Calendario
                 </a>
               </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-              <a href="{{ url('/logout') }}" class="btn btn-primary"> <i class="fa fa-door-open"></i></a>
+              <a href="{{ url('/logout') }}" class="btn btn-primary"> <i class="fa fa-door-open"></i> Logout</a>
             </form>
           </div>
       </nav>
@@ -44,6 +47,9 @@
    
 
 </body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/6adde2d540.js" crossorigin="anonymous"></script>
 <script type="text/javascript">
     var btn = document.getElementById('btn-div');
